e1c217ffaed322277256c2ea358661d6fc4549c1
André Lucas <andre.lucas.lemos@hotmail.com>
2023-01-25 17:20:41 -0300

    feat: Ajustes nos componentes de botões e textos

--- src/shared/components/PrimaryButton/PrimaryButton.tsx
+++ src/shared/components/PrimaryButton/PrimaryButton.tsx
@@ -1,29 +1,47 @@
+/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
 import React from "react";
 import styled from "styled-components";
 
 interface ButtonProps {
-  text?: string;
   handleClick?: () => void;
+  children: React.ReactNode;
+  fontSize?: string;
+  width?: string;
+  height?: string;
+  marginBottom?: string;
 }
 
-const Button = styled.button`
-  width: 150px;
+const Button = styled.button<ButtonProps>`
+  font-size: 18px;
+  font-weight: 400;
+  width: auto;
+  min-width: 150px;
   height: 50px;
-  border-radius: 8px;
+  border-radius: 12px;
   border: none;
   cursor: pointer;
+  margin-bottom: ${(props) => props.marginBottom};
   color: ${(props) => props.theme.colors.white};
   background: ${(props) => props.theme.colors.primary};
-  font-weight: bold;
   &:hover {
     background-color: #4362ab;
   }
 `;
 
-export function PrimaryButton({ text, handleClick }: ButtonProps) {
+export function PrimaryButton({
+  children,
+  handleClick,
+  height,
+  width,
+  fontSize,
+}: ButtonProps) {
   return (
-    <Button type="submit" onClick={handleClick}>
-      {text}
+    <Button
+      type="submit"
+      onClick={handleClick}
+      style={{ height, minWidth: width, fontSize }}
+    >
+      {children}
     </Button>
   );
 }
--- src/shared/components/Text/Text.tsx
+++ src/shared/components/Text/Text.tsx
@@ -3,12 +3,15 @@ import styled from "styled-components";
 interface TextProps {
   fontSize: number;
   fontWeight: number;
+  color?: string;
+  marginTop?: string;
 }
 
 const Text = styled.h1<TextProps>`
   font-size: ${(props) => `${props.fontSize}px`};
   font-weight: ${(props) => props.fontWeight};
-  color: ${(props) => props.theme.colors.black};
+  color: ${(props) => (props.color ? props.color : props.theme.colors.black)};
+  margin-top: ${(props) => props.marginTop};
 `;
 
 export default Text;
